c0ad434e0dbcd0cfa3964c81a711f5f57bb0572b
Ethan Dritz <dritzethan@gmail.com>
2019-11-16 23:36:22 -0500

    Fixed F value calculation and added testing functions

--- aStar.py
+++ aStar.py
@@ -8,9 +8,11 @@ class HashTable:
 
     def __init__(self):
         self.ht = {}
+        self.size = 0
 
     def add(self, node):
         self.ht[node.position.xVal + math.sqrt(node.position.yVal)] = node
+        self.size += 1
 
     def hasKey(self, node):
         try:
@@ -19,10 +21,10 @@ class HashTable:
             return None
 
 
-
 class PriorityQueue(object):
     def __init__(self):
         self.queue = []
+        self.size = 0
 
     def __str__(self):
         return ' '.join([str(i) for i in self.queue])
@@ -36,30 +38,48 @@ class PriorityQueue(object):
 
     def insert(self, data):
         self.queue.append(data)
+        self.size += 1
 
         # for popping an element based on Priority
 
     def delete(self):
         try:
-            max = 0
+            minimum = 0
             for i in range(len(self.queue)):
-                if self.queue[i] > self.queue[max]:
-                    max = i
-            item = self.queue[max]
-            del self.queue[max]
+                if self.queue[i].fVal < self.queue[minimum].fVal:
+                    minimum = i
+            item = self.queue[minimum]
+            del self.queue[minimum]
+            self.size -= 1
             return item
         except IndexError:
             print()
             exit()
 
+
 class grid():
-    yUpperBound = 10
-    yLowerBound = 0
-    xUpperBound = 10
-    xLowerBound = 0
+
+    def __init__(self, xUpperBound, xLowerBound, yUpperBound, yLowerBound):
+        self.xUpperBound = xUpperBound
+        self.xLowerBound = xLowerBound
+        self.yUpperBound = yUpperBound
+        self.yLowerBound = yLowerBound
+        self.arr = [[0 for i in range(xLowerBound, xUpperBound + 1)] for j in range(yLowerBound, yUpperBound + 1)]
+        self.buildMap()
+
+    def buildMap(self):
+        mapFile = open("map.txt", 'r')
+        for row in range(self.yLowerBound, self.yUpperBound):
+            line = mapFile.readline()
+            for column in range(self.xLowerBound, self.xUpperBound):
+                self.arr[column][row] = int(line[column])
+        mapFile.close()
+        #print(self.arr)
 
     def isBarrier(self, position):
-        asdf = 1
+        if self.arr[position.xVal][position.yVal] == 1:
+            return True
+
     def withinBounds(self, position):
         if position.xVal > self.xUpperBound or position.yVal > self.yUpperBound:
             return False
@@ -80,38 +100,70 @@ class starSearch():
         self.start = start
         self.end = end
         self.grid = grid
+        self.foundGoal = False
 
     class Node():
-        def __init__(self, fVal, position, level, parent):
+        def __init__(self, fVal, gVal, position, level, parent):
             self.fVal = fVal
+            self.gVal = gVal
             self.position = position
             self.level = level
             self.parent = parent
 
     def findPath(self):
-        self.openList.insert(self.Node(0, self.start, 1, None))  # inserts root node to open list
-        while not self.openList.isEmpty():
-            self.sucessors(self.openList.delete())
+        self.openList.insert(self.Node(0, 0, self.start, 0, None))  # inserts root node to open list
+        searchCount = 0
+        while not self.openList.isEmpty() and self.foundGoal is False:
+            searchCount += 1
+            self.successors(self.openList.delete())
+        if self.foundGoal is True:
+            print("Number of searches: {}".format(searchCount))
 
     def generateFVal(self, node, currentPosition, newPosition):
-        return self.generateHVal(currentPosition, newPosition) + self.generateGVal(node.fVal)
+        return self.generateHVal(currentPosition, newPosition) + self.generateGVal(node.gVal)
 
     def generateHVal(self, currentPosition, newPosition):
-        return math.sqrt(math.pow((newPosition.xVal - currentPosition.xVal), 2) + math.pow((newPosition.yVal - currentPosition.yVal), 2))
+        return math.sqrt(math.pow((self.end.xVal - newPosition.xVal), 2) + math.pow((self.end.yVal - newPosition.yVal), 2))
 
     def generateGVal(self, previousVal):
         return previousVal + 1
 
     def validPosition(self, position):
-        if grid.withinBounds(position) and not grid.isBarrier(position):
+        if self.grid.withinBounds(position) and not self.grid.isBarrier(position):
+            return True
+        else:
+            return False
+        
+    def unvisited(self, node):
+        if self.closedList.hasKey(node) is not None:
+            return False
+        else:
             return True
 
-    def sucessors(self, node):
-        for i in range(0, 3):
-            for j in range(0, 3):
+    def isGoal(self, node):
+        if node.position.xVal is self.end.xVal and node.position.yVal is self.end.yVal:
+            print("Number of moves: {}".format(node.level))
+            self.printPath(node)
+            return True
+        return False
+
+    def successors(self, node):
+        if self.isGoal(node):
+            self.foundGoal = True
+            return
+        self.closedList.add(node)
+        for i in range(-1, 2):
+            for j in range(-1, 2):
                 newPosition = coordinate(node.position.xVal + i, node.position.yVal + j)
-                if self.validPosition(newPosition):
-                    self.openList.insert(self.Node(self.generateFVal(node, node.position, newPosition), newPosition, node.level + 1, node))
+                newNode = self.Node(self.generateFVal(node, node.position, newPosition), node.gVal, newPosition, node.level + 1, node)
+                if self.validPosition(newPosition) and self.unvisited(newNode):
+                    self.openList.insert(newNode)
+
+    def printPath(self, node):
+        nextNode = node
+        while nextNode.parent is not None:
+            print("({},{})".format(nextNode.position.xVal, nextNode.position.yVal))
+            nextNode = nextNode.parent
 
 
 
@@ -120,11 +172,10 @@ class starSearch():
 
 
 if __name__ == '__main__':
-    myQueue = PriorityQueue()
-    myQueue.insert(12)
-    myQueue.insert(1)
-    myQueue.insert(14)
-    myQueue.insert(7)
-    print(myQueue)
-    while not myQueue.isEmpty():
-        print(myQueue.delete())
+    ol = PriorityQueue()
+    cl = HashTable()
+    gr = grid(10, 0, 10, 0)
+    searcher = starSearch(ol, cl, coordinate(5, 5), coordinate(8, 3), gr)
+    searcher.findPath()
+    print("Open list size: {}".format(ol.size))
+    #print(cl.size)
